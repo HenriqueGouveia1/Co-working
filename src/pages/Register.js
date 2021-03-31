bdb291a18865f163e03061d028f7675be3631a57
HenriqueGouveia1 <henriquegs002@gmail.com>
2021-03-31 14:48:20 -0300

    Registro atualizado

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -9,28 +9,40 @@ function Register() {
             <div className="mb-3">
                 <input type="email" placeholder="Email" className="form-control btn btn-primary" id="formEmail"/>
             </div>
+            
             <div className="mb-3">
-                <input type="text" placeholder="Você é" className="form-control btn btn-primary" id="formYoure"/>
+                <select placeholder="Você é" className="form-control btn btn-primary" id="formYoure">
+                <option hidden >Você é...</option>
+                <option value="Estudante">Estudante</option>
+                <option value="Interpreté">Interpreté</option>
+                <option value="Professor">Professor</option>
+                <option value="Outros">Outros</option>
+                </select>
+                
             </div>
             <div className="mb-3">
                 <input type="password" placeholder="Senha" className="form-control btn btn-primary" id="formPassword"/>
             </div>
             <div className="mb-3">
-                <input type="password" placeholder="Confirma Senha" className="form-control btn btn-primary" id="formRePassword"/>
-                <br></br>
-                <br></br>
-                <br></br>
-                <h4 className="mt-60 mb-30" class="data">Data de Nascimento</h4>
-                <div className="mb-3">
-                <input type="text" placeholder="JAN" className="form-control btn btn-primary" id="formUsername"/>
+                <input type="password" placeholder="Confirma Senha" className="form-control btn btn-primary mb-30" id="formRePassword"/>
+                </div>     
+                
+                <div className="ml-60 mr-60 mb-30">
+                <h4  class="data">Data de Nascimento</h4>
+                </div>
+            
+            <div class="grid-container">
+                <div id= "item1" class="grid-item" className="ml-30 mr-30 mb-3">
+                    <input type="text" placeholder="JAN" className=" form-control btn btn-primary" id="formUsername"/>
+                </div>
+                <div id= "item2" class="grid-item" className="mr-30 mb-3">
+                    <input type="number" placeholder="01" className="form-control btn btn-primary" id="formUsername"/>
+                </div>
+                <div id= "item3" class="grid-item" className="mr-30 mb-3">
+                    <input type="number" placeholder="2000" className="form-control btn btn-primary" id="formUsername"/>
+                </div>
             </div>
-            <div className="mb-3">
-                <input type="number" placeholder="01" className="form-control btn btn-primary" id="formUsername"/>
-            </div>
-            <div className="mb-3">
-                <input type="number" placeholder="2000" className="form-control btn btn-primary" id="formUsername"/>
-            </div>
-            </div>
+
             <button type="submit" className="btn btn-primary mt-5 mb-4">Criar Conta</button>
         </form>
     <p>Ao se cadastrar, você concorda com os Termos de Serviço e a Política de Privacidade. </p>
