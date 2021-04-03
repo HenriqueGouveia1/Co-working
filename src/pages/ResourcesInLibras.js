82dd3147e06c54a5183de2bfbebee2bce857c494
HenriqueGouveia1 <henriquegs002@gmail.com>
2021-04-03 15:13:37 -0300

    alterações no resourcelibras e css e imagens adicionadas

--- src/pages/ResourcesInLibras.js
+++ src/pages/ResourcesInLibras.js
@@ -1,4 +1,5 @@
 import NavSearchBar from "../components/NavSearch";
+import NavFooter from "../components/NavFooter";
 
 function ResourcesInLibras() {
     return (
@@ -7,30 +8,53 @@ function ResourcesInLibras() {
             <NavSearchBar />
             <div className="mt-120">
                 <h1 id="wrapperTitle" className="" >Apresentação dos Ícones</h1>
-                <div className="grid-container">
+                <div className="grid-container ml-60 mt-3">
                     <div id="item1" className="grid-item">
-                        
-                        
+                        <div className="ml-60"><img className="" src="./img/apresentacaohome.svg" alt="Inicio" />
+                        <p>Inicio</p>  </div>
 
+                        <p id="videoemlibras">Video em libras</p>
                     </div>
-                    <div id="item2"  className="grid-item">
-                        content 2
-                    </div>
-                    <div className="grid-item">
-                        content 3
-                    </div>
-                    <div className="grid-item">
-                        content 4
-                    </div>
-                    <div className="grid-item">
-                        content 5
+                    <div id="item2" className="grid-item">
+                       <div className="ml-60">
+                       <img src="./img/apresentacaofav.svg" alt="Favoritos" />
+                        <p>Favoritos</p>
+                       </div>
+                        <p id="videoemlibras">Video em libras</p>
                     </div>
                     <div className="grid-item">
-                        content 6
+                       <div className="ml-60">
+                       <img src="./img/LIBRAS.svg" alt="libras" />
+                        <p>Itens em LIBRAS</p>
+                       </div>
+
+                        <p id="videoemlibras">Video em libras</p>
+                    </div>
+                    <div className="grid-item">
+                       <div className="ml-60">
+                       <img src="./img/OPCOES_1.PNG" alt="opcoes" />
+                        <p>Opções</p>
+                       </div>
+                        <p id="videoemlibras">Video em libras</p>
+                    </div>
+                    <div className="grid-item">
+                        <div className="ml-60">
+                        <img src="./img/apresentacaoengrenagem.svg" alt="engrenagem" />
+                        <p>Configurações</p>
+                        </div>
+                        <p id="videoemlibras">Video em libras</p>
+                        
+                    </div>
+                    <div className="grid-item">
+                       <div className="ml-60">
+                       <img src="./img/pergunta 4.png" alt="ajuda" />
+                        <p>Ajuda</p>
+                        <p id="videoemlibras">Video em libras</p>
+                       </div>
                     </div>
                 </div>
             </div>
-
+            <NavFooter/>
         </div>
 
     );
