19ea6a02e10cf2dcb55d7ae005699cc9f4787529
HenriqueGouveia1 <henriquegs002@gmail.com>
2021-04-07 23:54:57 -0300

    tentativa de alinhar o bagui quase que falha por henriquegouveia triste

--- src/pages/Libras.js
+++ src/pages/Libras.js
@@ -5,93 +5,112 @@ function Libras() {
 
         <div className="">
             <NavSearch />
-            
+
             <div className="mt-120 mb-60">
                 <h1 className="wrapperTitle" >Apresentação dos Ícones</h1>
-                <div className="grid-container ml-60 mt-3">
-                <div className="grid-item item-2">
-                <div className="container">
-                  
-                   <div className="mt-30 mr-30 mb-3" >
-                        <img src="img/homeApresentacao.svg" alt="a"/>
-                        <p>Inicio</p>
-                        <br />
-                        <iframe width="auto" height="auto" 
-                        src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0" 
-                        title="YouTube video player" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen></iframe>
-                        </div> 
-                    
+                <div className="grid-container">
+                    <div className="container">
+                    <div className="row">     
+                        <div className="col-3 mr-120">    
+                            <div className=" mt-30 mr-30 mb-3" >
+                                <div className="librasTitle">
+                                    <img src="img/homeApresentacao.svg" alt="a" />
+                                    <p>Inicio</p>
+                                </div>
+                                <br />
+                                <iframe width="auto" height="auto"
+                                    src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
 
+                            <div className="mt-30 mr-30 mb-3">
+                                <div className="librasTitle">
+                                    <img src="img/favApresentacao.svg" alt="a" />
+                                </div>
+
+                                <p>Favoritos</p>
+                                <br />
+                                <iframe width="auto" height="auto"
+                                    src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+
+                            <div className="mt-30 mr-30 mb-3"  >
+                                <div className="librasTitle">
+                                    <img src="img/librasApresentacao.svg" alt="a" />
+                                    <p>Itens em LIBRAS</p>
+                                </div>
+
+                                <br />
+                                <iframe width="auto" height="auto"
+                                    src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+
+
+                        </div>
+                    <div className="col-3 ml-60">
                     <div className="mt-30 mr-30 mb-3"  >
-                        <img src="img/favApresentacao.svg" alt="a"/>
-                        <p>Favoritos</p>
-                        <br />
-                        <iframe width="auto" height="auto" 
-                        src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0" 
-                        title="YouTube video player" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen></iframe>
-                    </div>
+                                <div className="librasTitle">
+                                    <img src="img/menuApresentacao.svg" alt="a" />
+                                    <p>Opções</p>
+                                </div>
 
-                        
-                    <div className="mt-30 mr-30 mb-3"  >
-                        <img src="img/librasApresentacao.svg" alt="a"/>
-                        <p>Intens em LIBRAS</p>
-                        <br />
-                        <iframe width="auto" height="auto" 
-                        src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0" 
-                        title="YouTube video player" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen></iframe>
-                    </div>
+                                <br />
+                                <iframe width="auto" height="auto"
+                                    src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
 
+                            <div className="mt-30 mr-30 mb-3"  >
+                                <div className="librasTitle">
+                                    <img src="img/condigApresentacao.svg" alt="a" />
+                                    <p>Configurações</p>
+                                </div>
 
-                    <div className="mt-30 mr-30 mb-3"  >
-                        <img src="img/menuApresentacao.svg" alt="a"/>
-                        <p>Opções</p>
-                        <br />
-                        <iframe width="auto" height="auto" 
-                        src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0" 
-                        title="YouTube video player" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen></iframe>
-                    </div>
-                    
-                    <div className="mt-30 mr-30 mb-3"  >
-                        <img src="img/condigApresentacao.svg" alt="a"/>
-                        <p>Configurações</p>
-                        <br />
-                        <iframe width="auto" height="auto" 
-                        src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0" 
-                        title="YouTube video player" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen></iframe>
-                    </div>
+                                <br />
+                                <iframe width="auto" height="auto"
+                                    src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
 
-                    <div className="mt-30 mr-30 mb-3"  >
-                        <img src="img/helpApresentacao.svg" alt="a"/>
-                        <p>Ajuda</p>
-                        <br />
-                        <iframe width="auto" height="auto" 
-                        src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0" 
-                        title="YouTube video player" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                        allowfullscreen></iframe>
-                    </div>
+                            <div className="mt-60 mr-30 mb-3">
+                               
+                               <div className="librasTitle">
+                                    <img src="img/helpApresentacao.svg" alt="a" />
+                                    <p>Ajuda</p>
+                                </div>
 
+                                <br />
+                                <iframe width="auto" height="auto"
+                                    src="https://www.youtube.com/embed/_yf5vzZ2sYE?controls=0"
+                                    title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div> 
+                    </div>       
+
+                        </div>
                     </div>
-                    </div>
-                    </div>
+                </div>
             </div>
-            <NavFooter/>
+            <NavFooter />
         </div>
 
     );
